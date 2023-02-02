62bd799214bd25ab660703a233974a95d24e2383
Aitor Nain Mendoza Vallejo <aitornainmendozavallejo@gmail.com>
2023-02-02 14:00:35 +0100

    Update

--- src/main/java/com/ShoppingList/demo/controles/RecipesController.java
+++ src/main/java/com/ShoppingList/demo/controles/RecipesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ShoppingList.demo.dto.RecipeDTO;
 import com.ShoppingList.demo.service.RecipesServices;
@@ -26,5 +27,12 @@ public class RecipesController {
 		
 	}
 
+	@GetMapping("/add-ingredients")
+	public String addIngredientsByIdMeal(@RequestParam("idMeal")int idMeal) {
+		
+		RecipeDTO recipe= recipeService.getIndregientsByIdMeal(idMeal);
+		
+		return"pLista";
+	}
 
 }
--- src/main/java/com/ShoppingList/demo/service/RecipesServices.java
+++ src/main/java/com/ShoppingList/demo/service/RecipesServices.java
@@ -1,39 +1,52 @@
-package com.ShoppingList.demo.service;
-
-
-
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import com.ShoppingList.demo.dto.RecipeDTO;
-import com.ShoppingList.demo.dto.RecipeResponseDTO;
-import com.google.gson.Gson;
-
-
-@Service
-public class RecipesServices {
+	package com.ShoppingList.demo.service;
 	
-	private final String urlMealApiRandom="https://www.themealdb.com/api/json/v1/1/random.php";
 	
-	public RecipeDTO getRecipeFromApi(){
-		
-		RestTemplate restT= new RestTemplate();
-		String result= restT.getForObject(urlMealApiRandom, String.class);
-		
-		Gson gson = new Gson();
-		
-		RecipeResponseDTO recipes= gson.fromJson(result, RecipeResponseDTO.class);
-		return recipes.getMeals().get(0);
-		
-	}
 	
-	public ArrayList<RecipeDTO> getListRecipesRandom(){
-		ArrayList<RecipeDTO> recipes= new ArrayList<RecipeDTO>();
-		for(int i=0;i<5;i++) {
-			recipes.add(getRecipeFromApi());
+	import java.util.ArrayList;
+	
+	import org.springframework.stereotype.Service;
+	import org.springframework.web.client.RestTemplate;
+	
+	import com.ShoppingList.demo.dto.RecipeDTO;
+	import com.ShoppingList.demo.dto.RecipeResponseDTO;
+	import com.google.gson.Gson;
+	
+	
+	@Service
+	public class RecipesServices {
+		
+		private final String urlMealApiRandom="https://www.themealdb.com/api/json/v1/1/random.php";
+		private final String urlMealApiByIdMeal="www.themealdb.com/api/json/v1/1/lookup.php?i=";
+		
+		public RecipeDTO getRecipeFromApi(){
+			
+			RestTemplate restT= new RestTemplate();
+			String result= restT.getForObject(urlMealApiRandom, String.class);
+			
+			RecipeResponseDTO recipes=getResponseByString(result);
+
+			return recipes.getMeals().get(0);
+			
 		}
-		return recipes;
-}
-}
+		
+		public ArrayList<RecipeDTO> getListRecipesRandom(){
+			ArrayList<RecipeDTO> recipes= new ArrayList<RecipeDTO>();
+			for(int i=0;i<5;i++) {
+				recipes.add(getRecipeFromApi());
+			}
+			
+			return recipes;
+	}
+		public RecipeDTO getIndregientsByIdMeal(int idMeal) {
+			RestTemplate restT= new RestTemplate();
+			String result= restT.getForObject(urlMealApiByIdMeal+idMeal, String.class);
+			RecipeResponseDTO recipe= getResponseByString(result);
+			return recipe.getMeals().get(0);
+		}
+		
+		public RecipeResponseDTO getResponseByString(String result) {
+			Gson gson= new Gson();
+			RecipeResponseDTO recipes=gson.fromJson(result, RecipeResponseDTO.class);
+			return recipes;
+		}
+	}
